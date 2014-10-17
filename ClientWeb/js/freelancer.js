523443c997f7d9da584f05f4e4cdad6f5cde905b
jsabbatier <justine.sabbatier@imerir.com>
2014-10-17 10:43:11 +0200

    Signed-off-by: jsabbatier <justine.sabbatier@imerir.com>
    Version finale

--- ClientWeb/js/freelancer.js
+++ ClientWeb/js/freelancer.js
@@ -1,8 +1,11 @@
 
  
-var monJSON = eval("[[[30,50],[120,70]],[[200,250],[200,290]],[[110,50],[150,120]]]");
+//var monJSON = eval("[[[30,50],[120,70]],[[200,250],[200,290]],[[110,50],[150,120]]]");
 var x;
 var y;
+var canvas;
+var context;
+
 
  
  /// @def data Json 
@@ -12,20 +15,11 @@ var data;
 /// @brief Courte description.ajax fonction for connecting supervisor and server
 /// @param method; here function jsonTable(data)		
 /// @param method; here function mapCoord(data)
-/*
-function comServer(method, method2)
-{
-	  $.ajax({
-       url :$('#adress').val(), // La ressource ciblée
-       type : 'GET', // Le type de la requête HTTP.
-	   statusCode : {
-		   404 : function (){alert( "Server offline")}
-	   }
-    }).done(function( msg ){method(msg);method2(msg)});
-   }*/
+   
 
-function JsonCoord(context)
+function JsonCoord( monJSON)
 {
+	monJSON = eval(monJSON);
 	for (var k in monJSON)
 	{
 		x=(monJSON[k][0][0]);
@@ -36,28 +30,41 @@ function JsonCoord(context)
 		context.lineTo(x, y);
 		console.log(x);
 	}
-}  
+	context.stroke();//On trace seulement les lignes.
+	context.closePath();
+} 
+
+function comServer(method)
+{
+	  $.ajax({
+       url :$('#adress').val(), // La ressource ciblée
+       type : 'GET', // Le type de la requête HTTP.
+	   statusCode : {
+		   404 : function (){alert( "Server offline")}
+	   }
+    }).done(function( msg ){method(msg);});
+   } 
 
 window.onload = function()
 {	
-    var canvas = document.getElementById('myCanvas');
+     canvas = document.getElementById('myCanvas');
         if(!canvas)
         {
             alert("Impossible de récupérer le canvas");
             return;
         }
 
-    var context = canvas.getContext('2d');
+     context = canvas.getContext('2d');
         if(!context)
         {
             alert("Impossible de récupérer le context du canvas");
             return;
         }
 		
+		
+		initialize(canvas, context);
 		context.beginPath();//On démarre un nouveau tracé
-		JsonCoord(context);
-		context.stroke();//On trace seulement les lignes.
-		context.closePath();
+		comServer(JsonCoord);
 
 
 }
@@ -82,6 +89,37 @@ function downloadCanvas(link, canvasId, filename) {
 document.getElementById('download').addEventListener('click', function() {
     downloadCanvas(this, 'myCanvas', 'carte.png');
 }, false)
+
+function initialize() {
+// Register an event listener to
+// call the resizeCanvas() function each time 
+// the window is resized.
+window.addEventListener('resize', resizeCanvas, false);
+
+// Draw canvas border for the first time.
+resizeCanvas();
+}
+
+function redraw() {
+		context.strokeStyle = '#2c3e50';
+		context.lineWidth = '5';
+		context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+            }
+
+function resizeCanvas() 
+{
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	redraw();
+}
+
+$("#connect").click(function (e)
+{
+	e.preventDefault();
+	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	context.beginPath();//On démarre un nouveau tracé
+	comServer(JsonCoord);
+});
 
 
 /************EFFETS SITE***********/
