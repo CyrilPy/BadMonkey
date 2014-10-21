66c4c08197fa1d7f0115dbbf0ca95c0fc1559753
Julien Loeve <julien.loeve@imerir.com>
2014-10-21 08:45:46 +0200

    Signed-off-by: Julien Loeve <julien.loeve@imerir.com>

--- web services/getMaps.php
+++ web services/getMaps.php
@@ -38,18 +38,18 @@ $dsn = "mysql:host=$hostname;dbname=$database";
 		
 		foreach ($row as $tmp )
 		{
-			$xRobot = (int)$tmp['xRobot'];
-			$yRobot = (int)$tmp['yRobot']; 
-			$xMap   = (int)$tmp['xMap'];
-(			$alpha  = (int)$tmp['alphaRobot'];
-			$date   = (int)$tmp['time'];
+			$xRobot = $tmp['xRobot'];
+			$yRobot = $tmp['yRobot']; 
+			$xMap   = $tmp['xMap'];
+(			$alpha  = $tmp['alphaRobot'];
+			$date   = $tmp['time'];
 			if($firstIteration)
 			{
-				$jsonRobot= array($xRobot,$yRobot,$alpha);
+				$jsonRobot= array((int)$xRobot,(int)$yRobot,(int)$alpha);
 				$firstIteration= false;
 			}
 
-			$jsonMapPoint[] = array($xMap,$yMap);
+			$jsonMapPoint[] = array((int)$xMap,(int)$yMap);
 
 		}
 
